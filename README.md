0bf4923d47272dbd87528482167dbb876a575faf
vitor-perin <vitor-perin@outlook.com>
2022-05-21 01:07:39 -0300

    Readme edited, index and script created, code for game completed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-rock-paper-scissors
+
+Rock, Paper and Scissors game project. Play in console, GUI will be added later on.
---
+++ script.js
@@ -0,0 +1,78 @@
+function computerPlay() {
+    const items = [`Rock`, `Paper`, `Scissors`];
+    const choice = items[Math.floor(Math.random()*items.length)];
+    return choice;
+}
+
+function playerPlay(str) {
+    const lower = str.toLowerCase();
+    return str.charAt(0).toUpperCase() + lower.slice(1);
+}
+
+function game() {
+    
+    let playerWin = 0;
+    let computerWin = 0;
+    
+    for (let roundsPlayed = 0; roundsPlayed < 5; roundsPlayed++) {
+        console.log(roundsPlayed + "batata")
+        const computerSelection = computerPlay();
+        const playerSelection = prompt(`Make your choice between Rock, Paper and Scissors:`, );
+        const playerInsensitive  = playerPlay(playerSelection);
+        const result = playRound(playerInsensitive, computerSelection);
+
+        function playRound(playerInsensitive, computerSelection) {
+            if (playerInsensitive === computerSelection) {
+                console.log("Tie");
+                roundsPlayed--;
+            }
+            else if (playerInsensitive == `Rock`){
+                if (computerSelection == `Paper`) {
+                    console.log("Computer Won");
+                    computerWin++;
+                    console.log(computerWin)
+                }
+                else {
+                    console.log("You won");
+                    playerWin++;
+                    console.log(playerWin)
+                }
+            }
+            else if (playerInsensitive == `Paper`){
+                if (computerSelection == `Scissors`) {
+                    console.log("Computer Won");
+                    computerWin++;
+                    console.log(computerWin)
+                }
+                else {
+                    console.log("You won");
+                    playerWin++;
+                    console.log(playerWin)
+                }
+            }
+            else if (playerInsensitive == `Scissors`){
+                if (computerSelection == `Rock`) {
+                    console.log("Computer Won");
+                    computerWin++;
+                    console.log(computerWin)
+                }
+                else {
+                    console.log("You won");
+                    playerWin++;
+                    console.log(playerWin)
+                }
+            }
+        }
+    }
+
+    if (playerWin > computerWin) {
+        console.log("Congrats you beat the Computer");
+    }
+    else {
+        console.log("Computer Won, Good luck next time!");
+    }
+
+}
+
+alert("Let's play Rock, Paper and Scissors!");
+game()
